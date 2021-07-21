c01e5d10088d98a01843e29063172d658983f18a
Don Fadel <don@kidona.com>
2021-07-21 16:17:42 -0400

    Initial

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider = "aws" {
+    profile = "use-1"
+    region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+    most_recent = true
+
+    filter {
+        name = "name"
+        values = [ "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*" ]
+    }
+
+    owners = [ "099720109477" ]
+}
+
+resource "aws_instance" "helloworld" {
+    ami = data.aws_ami.ubuntu.id
+    instance_type = "t2.micro"
+}
+
